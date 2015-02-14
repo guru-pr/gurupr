bc908becf9443af980a38e968dee2e5f59e86e9a
Leonardo Saraiva <vyper@maneh.org>
2015-02-14 17:45:32 +0000

    Moved factory_girl config to support/

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,8 +25,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
