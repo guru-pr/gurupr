462a30784ed3a186ce013429c9379be6f19c7463
Adilson Carvalho <lc.adilson@gmail.com>
2015-02-18 14:12:35 -0200

    Added md rendering capability to the app decorator

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,2 +1,10 @@
 class ApplicationDecorator < Draper::Decorator
+  def renderer
+    @renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  end
+
+  def render_markdown(markdown)
+    return nil if markdown.blank?
+    renderer.render(markdown).html_safe
+  end
 end
