8c9d29f1be196addc632e05f052ce3463116b8d4
Leonardo Saraiva <vyper@maneh.org>
2015-04-25 12:03:52 -0300

    Changed timezone to 'America/Sao_Paulo'

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Gurupr
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Brasilia'
+    config.time_zone = 'America/Sao_Paulo'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
