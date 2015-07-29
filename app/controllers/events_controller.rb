4972289846268fa0ff72374d1149afac579e9618
Marcos Neves <marcos.neves@gmail.com>
2015-07-29 00:26:41 -0300

    Atom feed at /events.atom

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,10 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.order(occurred_at: :desc).decorate
+    @events = Event.order(occurred_at: :desc)
+    respond_to do |format|
+      format.html { @events = @events.decorate }
+      format.atom
+    end
   end
 
   def show
