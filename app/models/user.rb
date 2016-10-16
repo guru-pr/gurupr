f78e585f15c559a259eece858f3ec7f0eadd73ec
Leonardo Saraiva <vyper@maneh.org>
2016-10-15 22:25:34 -0300

    Add user data in sendgrid to send news and other comunications

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,4 @@ class User < ApplicationRecord
 
   scope :admin,  -> { where(admin: true) }
   scope :normal, -> { where(admin: false) }
-
-  def self.find_or_create_with_omniauth(auth)
-    user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
-    user.update name:     auth.info.name,
-                email:    auth.info.email,
-                nickname: auth.info.nickname,
-                image:    auth.info.image
-    user
-  end
 end
