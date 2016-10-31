97d19f99344453e059414e6edc878f952cae1918
Jake Yesbeck <yez@users.noreply.github.com>
2016-10-31 07:49:39 -0400

    Remove Draper (#126)
    * Remove Draper
    
    These changes remove the use of Draper in favor of a built in Ruby
      mechanism: DelegateClass. This combined with leveraging Rails' view_context
      object help keep responsibilities separated from decorator and view rendering.
    
    Draper was very invasive and as such, many specs had to change. Potentially
      controversial changes are evident in spec/decorators/event_decorator_spec.
      More specifically, specs have changed to expect messages onto the
      internal view_context for url and other html generation.
    
    A helper file MarkdownHelper was deleted and its logic in-lined to the atom
      response block in the events controller.
    
    Happy to explain/discuss these changes further.
    
    * Code review changes, remove decorate method from model
    
    Remove the decorate method from the Event model, have the controller
      initialize a decorator explicitly.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,13 +2,19 @@ class EventsController < ApplicationController
   def index
     @events = Event.order(occurred_at: :desc)
     respond_to do |format|
-      format.html { @events = @events.decorate }
-      format.atom
+      format.html { @events = @events.map { |e| EventDecorator.new(e, view_context) } }
+      format.atom { @markdown_renderer = markdown_renderer }
     end
   end
 
   def show
-    @event = Event.friendly.find(params[:id]).decorate
+    @event = EventDecorator.new(Event.friendly.find(params[:id]), view_context)
     @title = @event.name
+  end
+
+  private
+
+  def markdown_renderer
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, strikethrough: true)
   end
 end
