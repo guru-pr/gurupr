ad40589a97313cccea9f299dfb198884af9f93a3
Bruno Casali <brunoocasali@gmail.com>
2017-01-02 17:55:42 -0200

    Reuse the profile image link as a new helper

---
+++ app/helpers/member_helper.rb
@@ -0,0 +1,22 @@
+module MemberHelper
+  def image_tag_for(member, github_link: false)
+    options =
+      if github_link
+        { link: "https://github.com/#{member.nickname}", target: '_blank' }
+      else
+        { link: '#', target: nil }
+      end
+
+    content_to_link_by(member, options)
+  end
+
+  private
+
+  def content_to_link_by(member, options)
+    link_to options[:link], target: options[:target], title: member.name,
+            class: 'member', style: "background-image: url(#{member.image})" do
+
+      content_tag(:p, member.name, class: :name)
+    end
+  end
+end
---
+++ spec/helpers/member_helper_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe MemberHelper, type: :helper do
+  let(:member) { create(:user, name: 'User Name', nickname: 'nick_name') }
+
+  describe '#image_tag_for' do
+    it 'returns the content with link and target' do
+      result = image_tag_for(member, github_link: true)
+
+      expect(result).to match /target=\"_blank\"/
+      expect(result).to match /https:\/\/github.com\/nick_name/
+    end
+
+    it 'returns the content without a target and anchor' do
+      result = image_tag_for(member)
+
+      expect(result).to_not match /target=\"_blank\"/
+    end
+
+    it 'returns a link with background-image and user name as content' do
+      result = image_tag_for(member)
+
+      expect(result).to match /background-image/
+      expect(result).to match /title=\"User Name\"/
+    end
+  end
+end
